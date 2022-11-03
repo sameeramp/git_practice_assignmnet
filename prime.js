95892cae36c0041542aeb60056181721b71f1b7e
sameeramp <sameeramp2000@gmail.com>
2022-11-03 16:52:28 +0530

    commited to if(count==2){

--- prime.js
+++ prime.js
@@ -1,14 +1,15 @@
-let count=0;
+let factor=0;
 function checkprime(num){
-  for(let i=0;i<num;i++){
+  for(let i=1;i<=num;i++){
     if(num%i==0){
-      count++;
-    }if(count==2){
+      factor++;
+    }
+  }
+  if(count==2){
        return true;
     }else{
     return false;
     }
-  }
 }
 let result=checkprime(5);
 console.log(result)
